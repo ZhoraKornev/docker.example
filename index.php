2b030f90eee898ffaac62f38544bfc466684d268
Zhora Kornev <jora_kornev@i.ua>
2020-03-11 23:35:50 +0200

    Some fixes

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-#!/usr/bin/env php
-<?php
-
-if (!file_exists(dirname(__DIR__).'/vendor/symfony/phpunit-bridge/bin/simple-phpunit.php')) {
-    echo "Unable to find the `simple-phpunit.php` script in `vendor/symfony/phpunit-bridge/bin/`.\n";
-    exit(1);
-}
-
-if (false === getenv('SYMFONY_PHPUNIT_DIR')) {
-    putenv('SYMFONY_PHPUNIT_DIR='.__DIR__.'/.phpunit');
-}
-
-require dirname(__DIR__).'/vendor/symfony/phpunit-bridge/bin/simple-phpunit.php';
+<html>
+<head>
+    <title>Тестируем PHP</title>
+</head>
+<body>
+<?php echo '<p>Привет, мир!</p>'; ?>
+</body>
+</html>
