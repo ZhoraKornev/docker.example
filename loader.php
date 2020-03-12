8612334e2ce458f49cd376d753f9ca4ae1be2d1c
Serega <obuhov.s@toplyvo.app>
2020-03-12 09:34:28 +0200

    Minor changes

--- loader.php
+++ loader.php
@@ -1,2 +1,8 @@
 <?php
+// Full Debug
+//define( 'WP_DEBUG', true );
+//define( 'WP_DEBUG_DISPLAY', true );
+//@ini_set( 'display_errors', E_ALL );
+//error_reporting(E_ALL);
+
 require_once( HOST . DS.'Helpers/DotEnv.php' );
