d749e289bac56ed8fd5ac1ec6d8b6ecabfcaced6
serega <serega@local.tuf>
2022-11-20 21:26:45 +0200

    Symfony added 🏨

--- app/console.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DS', '/');
-define('HOST', dirname(__FILE__)); // Root path
-require_once HOST . DS . 'loader.php';
-system("clear"); //Очищаем экран перед выводом
-var_dump(HOST . DS);
-
-
-
--- app/loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Full Debug
-//@ini_set( 'display_errors', E_ALL );
-//error_reporting(E_ALL);
-
-var_dump(HOST . DS);
